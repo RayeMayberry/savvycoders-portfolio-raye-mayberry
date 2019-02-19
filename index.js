9d82d6ab4a8b46d12b5f15ce829803e0761a8cb0
Raye Mayberry <rayemayberry@yahoo.com>
2019-02-18 21:04:19 -0600

    refactor html into javascript

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /* eslint-disable linebreak-style */
+import Navigation from './src/Navigation';
+
 var name;
+var originalContent = document.body.innerHTML;
 
 var blankChecker = function blankChecker(){
     if(name === ''){
         name = prompt('name pls');
         blankChecker(); // putting this inside the if statement will break the loop if the condition is false, and let the code move on to the next stage
     }
+
+    // putting the blankChecker() recursion statement out here would allow the loop to keep going infinitely, checking whether the name variable is blank or not
 };
 
 var namePrompt = function namePrompt(){
@@ -17,3 +22,10 @@ var namePrompt = function namePrompt(){
 };
 
 namePrompt();
+
+
+document.body.innerHTML = `
+        ${Navigation}
+        ${originalContent}
+`
+;
---
+++ src/Navigation.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+export default `
+<div id="navigation">
+        <ul class="container">
+            <li><a href="/">Home</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li>
+                <a href="/projects">Projects</a>
+                <ul> <!--Dropdown menu-->
+                    <li><a href="">First</a></li>
+                    <li><a href="">Second</a></li>
+                    <li><a href="">Third</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+`;
