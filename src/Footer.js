643b22b7f33fe380eb9744f117f461d369d1a03f
raye <38679090+nascenticity@users.noreply.github.com>
2019-04-10 02:53:49 -0500

    tweak styles and content

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,11 @@
 export default function Footer(){
     return `
 <div id="footer">
+    <div class="container">
     <a>GitHub</a>
     <a>LinkedIn</a>
     <a>Twitter</a>
+    </div>
 </div>
 `;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,7 @@
 export default function Home(){
     return `
-        <p>Raye is a developer based in St. Louis, Missouri, USA. She built this site as a project for the SavvyCoders course.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit repudiandae ut, itaque cupiditate officiis laboriosam harum laborum quod suscipit fuga tenetur! Reiciendis aliquam similique iure exercitationem dolores dignissimos dolorem eos?</p>
-        <p>Natus sapiente, quam sequi hic ipsum minus voluptas alias, reprehenderit consectetur ex nostrum neque maiores facilis nobis amet modi necessitatibus veritatis itaque numquam ea impedit! Odio necessitatibus harum repudiandae reprehenderit?</p>
-        <p>At vero quo similique maxime voluptatum quisquam accusantium sunt repudiandae reiciendis, aliquid modi praesentium nobis corrupti voluptates doloremque officia dolorem ipsam ducimus suscipit laborum illo eligendi. Vero fuga quasi maxime?</p>
+        <p>Raye is a developer based in St. Louis, Missouri, USA. She built this site as a project for the SavvyCoders course in 2019.</p>
+        <p>Raye's interests include cats, video games, Dungeons and Dragons, and creative writing. She taught herself HTML/CSS in 2013 to make themes for her (now-defunct) Tumblr blog, and eventually enrolled in SavvyCoders to expand her skillset, particularly by learning JavaScript.</p> <p>Future plans are to learn WebGL for use with current projects, as well as continuing to explore graphics with Blender and game development tools.</p>
+        
     `;
 }
