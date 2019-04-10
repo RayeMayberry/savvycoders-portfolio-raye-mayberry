02c9bd3b7c8808247dd79f73a8664bf3715e4db1
raye <38679090+nascenticity@users.noreply.github.com>
2019-04-10 02:59:09 -0500

    fix broken div

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,7 @@ export default function Footer(){
     <div class="container">
     <a href="https://github.com/RayeMayberry">GitHub</a>
     <a href="https://www.linkedin.com/in/raye-mayberry-767570106/">LinkedIn</a>
+    </div>
 </div>
 `;
 }
