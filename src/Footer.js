d3038273e0efd6e2e5a627a91c28abb3538f1887
raye <38679090+nascenticity@users.noreply.github.com>
2019-04-10 02:57:49 -0500

    add links to footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,8 @@ export default function Footer(){
     return `
 <div id="footer">
     <div class="container">
-    <a>GitHub</a>
-    <a>LinkedIn</a>
-    <a>Twitter</a>
-    </div>
+    <a href="https://github.com/RayeMayberry">GitHub</a>
+    <a href="https://www.linkedin.com/in/raye-mayberry-767570106/">LinkedIn</a>
 </div>
 `;
 }
